4680cecf0dfb42bb6d40b4590e46e9bac210b985
Kostiantyn Zablotskyi <zabkosta@gmail.com>
2018-04-18 11:09:39 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,3 @@
+A laravel wrapper for lstrojny/functional-php
+
+v 0.1 Beta
---
+++ src/FunctionalProvider.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace zkosta\Functional;
+
+use Illuminate\Support\ServiceProvider;
+
+class FunctionalProvider extend ServiceProvider {
+	
+
+ /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {   
+      
+    }
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
